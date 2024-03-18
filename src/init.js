b55b3a3d93f81b869e7699c736336595e6a81b4f
Jongseo Won <jongseowon@Jongseos-MacBook-Pro.local>
2024-03-18 20:35:17 +0900

    a

--- src/init.js
+++ src/init.js
@@ -4,6 +4,6 @@ import app from "./server.js";
 const PORT = process.env.PORT || 4000;
 
 const handleListening = () =>
-  console.log(`✅ Server listening on http://localhost:${PORT}`);
+  console.log(`✅ Server listening on http://localhost:${PORT}!`);
 
 app.listen(PORT, handleListening);
